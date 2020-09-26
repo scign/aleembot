7d8565ba54aa9bf75acbcfb8770eae65a6a0572d
scign <scignspam.github@mailnull.com>
2020-09-26 19:42:48 -0400

    add event handler

--- app/views.py
+++ app/views.py
@@ -1,8 +1,19 @@
 # Author: Aleem Juma
 
-from flask import render_template, request
+from flask import render_template, request, Response
 from app import app
+from threading import Thread
 
 @app.route('/') 
 def home(): 
     return '<h1>Working!</h1>'
+
+@app.route('/slack/events')
+def handle_event():
+    try:
+        data = request.json
+        if data['token'] != app.config('VERIFICATION_TOKEN'):
+            return Response(status=403)
+        if data.get('type','') == 'url_verification':
+            return Response(status=200, {"challenge": data['challenge']})
+    return Response(status=500)
