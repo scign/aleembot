d59946db86728fcb727b6a0f446e1fc0a16d02e5
scign <scignspam.github@mailnull.com>
2023-09-14 18:28:15 -0400

    Update config.py
    Uncommented verification_token and client_id

--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SLACK_CLIENT_SECRET', '')
     SIGNING_SECRET = os.environ.get('SLACK_SIGNING_SECRET', '')
-    # VERIFICATION_TOKEN = os.environ.get('SLACK_VERIFICATION_TOKEN', '')
+    VERIFICATION_TOKEN = os.environ.get('SLACK_VERIFICATION_TOKEN', '')
     TOKEN = os.environ.get('SLACK_BOT_TOKEN', '')
-    # CLIENT_ID = os.environ.get('SLACK_CLIENT_ID', '')
+    CLIENT_ID = os.environ.get('SLACK_CLIENT_ID', '')
